7ba7b1e14b214722b2f8bdb34da4589beaec8344
k-romero <imkevinromero@gmail.com>
2020-02-16 17:08:39 -0500

    created 2 for loops to move forward and backward and compare values

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,35 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int result = 0;
+        int sk = startIndex;
+        int next = 0;
+        int prev = 0;
+
+
+        for (int i = sk; i < playList.length; i++) {
+            if(playList[i] != selection){
+                next++;
+            } else {break;}
+        }
+
+        for (int j = sk; j < playList.length; j--) {
+            if(j-1 < 0){
+                j = playList.length-1;
+                prev++;
+            }
+            if(playList[j] != selection){
+                prev++;
+            } else {break;}
+        }
+
+
+        if(next <= prev){
+            result = next;
+        } else result = prev;
+
+
+
+        return result;
     }
 }
--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -28,4 +28,16 @@ public class MusicTest {
         Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void selectionTest3(){
+
+        String[] playlist = {"dancinginthedark","rio","lol","liveoak","liveoak","liaskndl", "rio", "uhuhf","hjbvdkjb","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 6;
+        String selection = "liveoak";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
 }
